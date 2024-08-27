e0d616fc49cd3aaf90464dfe31cb2b9450da28c7
jongse7 <mmssjj72@naver.com>
2024-08-27 19:21:06 +0900

    feat: 상세 정보 페이지 추가, 위치 권한 요청 페이지 추가, 버그 개선, 그외 다수 기능 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:ssu_map_test/map/view/location_permission_screen.dart';
 import 'package:ssu_map_test/options/firebase_options.dart';
 import 'package:ssu_map_test/onboarding/view/splash_screen.dart';
 
